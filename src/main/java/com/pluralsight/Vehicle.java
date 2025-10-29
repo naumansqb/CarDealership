673bab9f7016ecee9eb310fc16308debfcfb248d
Nauman <naumansqb@gmail.com>
2025-10-29 01:00:04 -0400

    Removed unnecessary setter

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -11,14 +11,14 @@ public class Vehicle {
     private int odometer;
     private double price;
 
-    public Vehicle(int vin, int year, String make, String model, String type, String color, int mileage, double price) {
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
         this.vin = vin;
         this.year = year;
         this.make = make;
         this.model = model;
-        this.vehicleType = type;
+        this.vehicleType = vehicleType;
         this.color = color;
-        this.odometer = mileage;
+        this.odometer = odometer;
         this.price = price;
     }
 
@@ -26,63 +26,33 @@ public class Vehicle {
         return vin;
     }
 
-    public void setVin(int vin) {
-        this.vin = vin;
-    }
-
     public int getYear() {
         return year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
 
     public String getMake() {
         return make;
     }
 
-    public void setMake(String make) {
-        this.make = make;
-    }
 
     public String getModel() {
         return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
     }
 
     public String getVehicleType() {
         return vehicleType;
     }
 
-    public void setVehicleType(String vehicleType) {
-        this.vehicleType = vehicleType;
-    }
-
     public int getOdometer() {
         return odometer;
-    }
-
-    public void setOdometer(int odometer) {
-        this.odometer = odometer;
     }
 
     public String getColor() {
         return color;
     }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
     public double getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     @Override
