be3cc1c24b26cef6dba3000d384f370b6e6ddc06
Nauman <naumansqb@gmail.com>
2025-10-28 23:45:19 -0400

    Implemented saveDealership() - Rewrites entire file upon saving each time.

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -9,27 +9,27 @@ public class DealershipFileManager {
         File file = new File(FILE_NAME);
         if (!file.exists()) {
             System.out.println("No dealership file found. \n You will be prompted to enter details about your dealership very soon <3.");
-            return new Dealership(null, null, null);
+            return new Dealership("", "", "");
         }
+
+        Dealership ds;
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME));
             String line = "";
-            int lineNumber=2;
+            int lineNumber = 2;
 
-            String dealershipInfo= reader.readLine();
-            if(dealershipInfo ==null){
-                reader.close();
-                return new Dealership(null,null,null);
+            String dealershipInfo = reader.readLine();
+            if (dealershipInfo == null) {
+                ds = new Dealership("", "", "");
+            } else {
+                String[] dealershipTokens = dealershipInfo.split("\\|");
+                if (dealershipTokens.length != 3) {
+                    ds = new Dealership("", "", "");
+                } else {
+                    ds = new Dealership(dealershipTokens[0], dealershipTokens[1], dealershipTokens[2]);
+                }
             }
-            String[] dealershipTokens = dealershipInfo.split("\\|");
-            if (dealershipTokens.length != 3) {
-                System.out.println("Invalid dealership format. Please follow the prompts below to update the necessary values");
-                reader.close();
-                return new Dealership(null, null, null);
-            }
-            Dealership ds= new Dealership(dealershipTokens[0],dealershipTokens[1],dealershipTokens[2]);
-
             while ((line = reader.readLine()) != null) {
                 String[] vehicleTokens = line.split("\\|");
                 if (vehicleTokens.length == 8) {
@@ -59,10 +59,31 @@ public class DealershipFileManager {
         } catch (Exception e) {
             System.out.println("Error reading from file. ");
             e.printStackTrace();
-            return new Dealership(null,null,null);
+            return new Dealership(null, null, null);
         }
     }
 
     public void saveDealership(Dealership dealership) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
+            writer.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhoneNumber());
+            writer.newLine();
+
+            for (Vehicle vehicle : dealership.getAllVehicles()) {
+                writer.write(vehicle.getVin() + "|" +
+                        vehicle.getYear() + "|" +
+                        vehicle.getMake() + "|" +
+                        vehicle.getModel() + "|" +
+                        vehicle.getVehicleType() + "|" +
+                        vehicle.getColor() + "|" +
+                        vehicle.getOdometer() + "|" +
+                        vehicle.getPrice());
+                writer.newLine();
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error saving dealership to file");
+            e.printStackTrace();
+        }
     }
 }
